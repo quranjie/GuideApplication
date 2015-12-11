abfd2643d35053e5b38e26c0b09a8a4bec74fec8
quranjie <quzhiyong99@126.com>
2015-12-11 10:56:58 +0800

    developing

---
+++ app/src/main/java/com/typ/ranjie/guideapplication/ui/MainActivity.java
@@ -0,0 +1,33 @@
+package com.typ.ranjie.guideapplication.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.typ.ranjie.guideapplication.BaseActivity;
+import com.typ.ranjie.guideapplication.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void findViewById() {
+
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+}
